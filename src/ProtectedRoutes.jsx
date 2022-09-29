ce9287df6b39110ca434442fe2d98212875aa742
JhonJairoChala <88052012+JhonJairoChala@users.noreply.github.com>
2022-09-29 13:04:08 -0500

    Update ProtectedRoutes.jsx

--- src/ProtectedRoutes.jsx
+++ src/ProtectedRoutes.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router'
 import { Home } from './Component/Page/Home/Home'
+import { useNavigate } from 'react-router-dom'
 
 const useAuth = () => {
     const user = sessionStorage.getItem("usuario")
@@ -15,7 +16,7 @@ const useAuth = () => {
 
 const ProtectedRoutes = () => {
   const isAuth = useAuth();
-  return isAuth ? <Outlet/> : <Home/>;
+  return isAuth ? <Outlet/> : navigate("/");
 }
 
 export default ProtectedRoutes
